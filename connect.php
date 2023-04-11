c6a971a8bbc995fd02896daf3acd2f79eca0c608
SudharshanamBuddha <87000293+SudharshanamBuddha@users.noreply.github.com>
2023-04-11 22:33:26 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+$email=$_POST['email'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$enquiry=$_POST['enquiry'];
+
+$conn = new mysqli('localhost', 'root', '','tae');
+if($conn->connect_error){
+    die('Connection Failed: '.$conn->connect_error);
+}
+else{
+    $stmt=$conn->prepare("insert into project(email,name,phone,enquiry)
+        values(?, ?, ?, ?)");
+    $stmt->bind_param("ssis",$email,$name,$phone,$enquiry);
+    $stmt->execute();
+    echo "successful";
+    $stmt->close();
+    $conn->close();
+}
+?>
