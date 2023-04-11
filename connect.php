699f8b489b707e12158b7a0dc56471210670a564
SudharshanamBuddha <87000293+SudharshanamBuddha@users.noreply.github.com>
2023-04-11 23:08:33 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -18,4 +18,31 @@ else{
     $stmt->close();
     $conn->close();
 }
+
+$sql = "CREATE DATABASE tae";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+$table_name = "project";
+
+// Replace 'your_database_name' with your database name
+$dbname = "tae";
+$sql = "CREATE TABLE $table_name (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(30) NOT NULL,
+name VARCHAR(30) NOT NULL,
+phone INT(30) NOT NULL,
+email VARCHAR(50) NOT NULL
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table $table_name created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
 ?>
