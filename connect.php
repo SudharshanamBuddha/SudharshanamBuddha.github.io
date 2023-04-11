a7182d00c5ab89e99e9a26e2fee02d583c06c04e
SudharshanamBuddha <87000293+SudharshanamBuddha@users.noreply.github.com>
2023-04-11 22:53:15 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,6 +7,7 @@ $enquiry=$_POST['enquiry'];
 $conn = new mysqli('localhost', 'root', '','tae');
 if($conn->connect_error){
     die('Connection Failed: '.$conn->connect_error);
+    echo "some error";
 }
 else{
     $stmt=$conn->prepare("insert into project(email,name,phone,enquiry)
